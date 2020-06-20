749d978be7b83f8a4055dc9e69bbabac7df17026
Carlos Ruesta <carlos_ruesta@yahoo.es>
2020-06-20 19:30:24 -0300

    Refatoring Aula 05
    - Autenticando usuarios usando funções de criptografia moderna de PHP
    - Como gerar uma senha segura usando o algoritmo ARGON2I
    - Usar no código PHP a função password_verify($senhaPura, $senhaHash)
    - Como inserir dados com Doctrine através de SQL
         vendor/bin/doctrine dbal:run-sql "SQL STATEMENT;"
    - Como validar um email usando a função filter_input (FILTER_VALIDATE_EMAIL)

---
+++ src/Controller/RealizarLogin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Alura\Cursos\Controller;
+
+use Alura\Cursos\Entity\Usuario;
+use Alura\Cursos\Infra\EntityManagerCreator;
+use Doctrine\Common\Persistence\ObjectRepository;
+
+class RealizarLogin implements InterfaceControladorRequisicao
+{
+
+    /** @var ObjectRepository */
+    private $repositorioUsuarios;
+
+    public function __construct()
+    {
+        $entityManager = (new EntityManagerCreator())->getEntityManager();
+        $this->repositorioUsuarios = $entityManager->getRepository(Usuario::class);
+    }
+
+    public function processaRequisicao(): void
+    {
+        $email = filter_input(INPUT_POST,'email', FILTER_VALIDATE_EMAIL);
+
+        if (is_null($email) || $email === false) {
+            echo "O e-mail digitado não é um e-mail válido";
+            exit();
+        }
+
+        $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
+
+        /** @var  $usuario */
+        $usuario = $this->repositorioUsuarios->findOneBy(['email' => $email]);
+
+        if (is_null($usuario) || !$usuario->senhaEstaCorreta($senha)) {
+            echo "E-mail ou senha inválidos";
+            return;
+        }
+
+        header('Location: /listar-cursos');
+    }
+}
